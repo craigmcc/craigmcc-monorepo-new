b9273ed41b82111a2f29fb82992cf7a8993b9c17
Craig McClanahan <craigmcc@gmail.com>
2026-04-03 10:41:56 -0700

    Implement I18N provider required by react-aria-components.

--- apps/daisy-showcase/src/app/layout.tsx
+++ apps/daisy-showcase/src/app/layout.tsx
@@ -7,9 +7,13 @@
 import "@repo/daisy-ui/styles.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { headers } from "next/headers";
 import React from "react";
+import { isRTL } from "react-aria-components";
 
 // Internal Modules ----------------------------------------------------------
+
+import { Providers } from "@/app/providers";
 
 // Public Objects ------------------------------------------------------------
 
@@ -21,15 +25,19 @@ export const metadata: Metadata = {
   description: "Example components with DaisyUI and React Aria Components",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
                                      children,
                                    }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const acceptLanguage = (await headers()).get('accept-language');
+  const lang = acceptLanguage?.split(/[,;]/)[0] || 'en-US';
   return (
-    <html lang="en">
+    <html lang={lang} dir={isRTL(lang) ? 'rtl' : 'ltr'}>
     <body className={`${geistSans.variable} ${geistMono.variable}`}>
-      {children}
+      <Providers lang={lang}>
+        {children}
+      </Providers>
     </body>
     </html>
   );
